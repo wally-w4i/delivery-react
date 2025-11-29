3de5ecc81609ad5cb84264dfc53c3a4e1f04602e
Walter Gabriel Orozco <walter.gabriel.orozco@gmail.com>
2025-11-29 21:02:43 +0200

    init react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Clients from "./components/Clients";
+
+function App() {
+  return (
+    <>
+      <Clients />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Clients.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+interface Client {
+  id: number;
+  description: string;
+  address: string;
+}
+
+export default function Clients() {
+  const [clients, setClients] = useState<Client[]>([]);
+
+  useEffect(() => {
+    async function fetchClients() {
+      try {
+        const request = await fetch("http://localhost:3000/clients");
+        if (!request.ok) {
+          throw new Error("Failed to fetch clients");
+        }
+        const response = await request.json();
+        setClients(response);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchClients();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {clients.map((c) => {
+          return (
+            <li key={c.id}>
+              {c.description} : {c.address}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
