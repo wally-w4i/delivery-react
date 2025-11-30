23308c149756696f52acdcf30f4c4a66b25c6a31
Walter Gabriel Orozco <walter.gabriel.orozco@gmail.com>
2025-11-30 13:03:26 +0200

    feat: restructure app layout with sidebar navigation and new components
    - Added Sidebar component for navigation between different views.
    - Introduced new components: Delivery, Log, Report, and Settings.
    - Updated App component to manage current view state and render selected component.
    - Removed App.css file as styles were not needed.
    - Updated Clients component to use new styling.
    - Added react-icons dependency for icons in the sidebar.

---
+++ src/components/Delivery.tsx
@@ -0,0 +1,7 @@
+export default function Delivery() {
+  return (
+    <div className="flex-auto min-h-screen bg-zinc-900 text-white p-6">
+      <h1 className="text-4xl font-bold text-center mb-6">DELIVERY</h1>
+    </div>
+  );
+}
