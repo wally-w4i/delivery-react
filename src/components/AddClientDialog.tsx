b4aa4de4b4a990b005b1c84c8e0e0a0207460f17
wally-w4i <gloomygold.dev@gmail.com>
2025-12-02 11:42:41 +0200

    feat: Add google map position selector (#8)
    Co-authored-by: Walter Gabriel Orozco <walter.gabriel.orozco@gmail.com>

--- src/components/AddClientDialog.tsx
+++ src/components/AddClientDialog.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import type { Client, GpsPosition } from "../services/api.services";
+import { FaLocationCrosshairs } from "react-icons/fa6";
+import MapModal from "./MapModal";
 
 interface AddClientDialogProps {
   isOpen: boolean;
@@ -28,6 +30,7 @@ export default function AddClientDialog({
   const [longitude, setLongitude] = useState(
     client?.gpsPosition?.longitude || 0
   );
+  const [isMapModalOpen, setMapModalOpen] = useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -37,6 +40,19 @@ export default function AddClientDialog({
       gpsPosition: { latitude, longitude },
     };
     onSubmit(clientData);
+    setDescription("");
+    setAddress("");
+    setLatitude(0);
+    setLongitude(0);
+  };
+
+  const handleSetGpsLocation = () => {
+    setMapModalOpen(true);
+  };
+
+  const handleLocationSelect = (position: { lat: number; lng: number }) => {
+    setLatitude(position.lat);
+    setLongitude(position.lng);
   };
 
   if (!isOpen) {
@@ -44,108 +60,124 @@ export default function AddClientDialog({
   }
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-zinc-800 p-8 rounded-lg w-full max-w-md">
-        <h2 className="text-xl font-semibold mb-2">
-          {client ? "Edit Client" : "Add New Client"}
-        </h2>
-        <form onSubmit={handleSubmit}>
-          {error && <p className="text-red-500 mb-4">{error}</p>}
-          <div className="mb-4">
-            <label
-              htmlFor="description"
-              className="block text-sm font-medium text-zinc-400"
-            >
-              Description
-            </label>
-            <input
-              type="text"
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
-              required
-              disabled={isLoading}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="block text-sm font-medium text-zinc-400"
-            >
-              Address
-            </label>
-            <input
-              type="text"
-              id="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
-              required
-              disabled={isLoading}
-            />
-          </div>
-          <div className="flex gap-5">
+    <>
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="bg-zinc-800 p-8 rounded-lg w-full max-w-md">
+          <h2 className="text-xl font-semibold mb-2">
+            {client ? "Edit Client" : "Add New Client"}
+          </h2>
+          <form onSubmit={handleSubmit}>
+            {error && <p className="text-red-500 mb-4">{error}</p>}
             <div className="mb-4">
               <label
-                htmlFor="latitude"
+                htmlFor="description"
                 className="block text-sm font-medium text-zinc-400"
               >
-                Latitude
+                Description
               </label>
               <input
-                type="number"
-                step="0.00000000000000001"
-                id="latitude"
-                value={latitude}
-                onChange={(e) => setLatitude(parseFloat(e.target.value))}
+                type="text"
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
+                required
                 disabled={isLoading}
               />
             </div>
             <div className="mb-4">
               <label
-                htmlFor="longitude"
+                htmlFor="address"
                 className="block text-sm font-medium text-zinc-400"
               >
-                Longitude
+                Address
               </label>
               <input
-                type="number"
-                step="0.00000000000000001"
-                id="longitude"
-                value={longitude}
-                onChange={(e) => setLongitude(parseFloat(e.target.value))}
+                type="text"
+                id="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
                 className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
+                required
                 disabled={isLoading}
               />
             </div>
-          </div>
-          <div className="flex justify-end space-x-2">
-            <button
-              type="button"
-              onClick={onClose}
-              className="px-4 py-2 bg-gray-600 hover:bg-gray-700 rounded-md text-white font-medium"
-              disabled={isLoading}
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className={`px-4 py-2 ${
-                isLoading ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-700"
-              } rounded-md text-white font-medium`}
-              disabled={isLoading}
-            >
-              {isLoading
-                ? "Saving..."
-                : client
-                ? "Update Client"
-                : "Add Client"}
-            </button>
-          </div>
-        </form>
+            <div className="flex gap-5">
+              <div className="mb-4">
+                <label
+                  htmlFor="latitude"
+                  className="block text-sm font-medium text-zinc-400"
+                >
+                  Latitude
+                </label>
+                <input
+                  type="number"
+                  step="0.00000000000000001"
+                  id="latitude"
+                  value={latitude}
+                  onChange={(e) => setLatitude(parseFloat(e.target.value))}
+                  className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
+                  disabled={isLoading}
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="longitude"
+                  className="block text-sm font-medium text-zinc-400"
+                >
+                  Longitude
+                </label>
+                <input
+                  type="number"
+                  step="0.00000000000000001"
+                  id="longitude"
+                  value={longitude}
+                  onChange={(e) => setLongitude(parseFloat(e.target.value))}
+                  className="mt-1 block w-full p-2 bg-zinc-700 border border-zinc-600 rounded-md text-white"
+                  disabled={isLoading}
+                />
+              </div>
+              <div
+                className="border content-center align-middle px-3 mb-4 mt-6 rounded"
+                onClick={handleSetGpsLocation}
+              >
+                <FaLocationCrosshairs />
+              </div>
+            </div>
+            <div className="flex justify-end space-x-2">
+              <button
+                type="button"
+                onClick={onClose}
+                className="px-4 py-2 bg-gray-600 hover:bg-gray-700 rounded-md text-white font-medium"
+                disabled={isLoading}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className={`px-4 py-2 ${
+                  isLoading ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-700"
+                } rounded-md text-white font-medium`}
+                disabled={isLoading}
+              >
+                {isLoading
+                  ? "Saving..."
+                  : client
+                  ? "Update Client"
+                  : "Add Client"}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      <MapModal
+        isOpen={isMapModalOpen}
+        onClose={() => setMapModalOpen(false)}
+        onLocationSelect={handleLocationSelect}
+        initialPosition={
+          latitude && longitude ? { lat: latitude, lng: longitude } : undefined
+        }
+      />
+    </>
   );
 }
